0dd8a843bfe24e22c1e423da78604d5b7898babd
Ilya Devin <i.devin@creotech.org>
2021-11-11 02:26:18 +0400

    Login form

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,61 +1,290 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="page-header mt-5">
-        <div class="row">
-            <div class="col-lg-9 mb-5 mb-sm-0 mb-md-0">
-                <div class="section">
-                    <div class="section-title">
-                        <h1>@lang('posts.last_posts')</h1>
-                    </div>
-                    <div class="section-items main-carousel">
-                        @foreach($posts as $post)
-                            <div class="section-item carousel-cell">
-                                @include('posts/_card')
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 mb-2">
-                @include ('posts/_search_form')
 
-                <span class="d-sm-none d-lg-block d-md-block d-xl-block">
-                    @include('posts/_tags')
-                </span>
-            </div>
+
+
+
+    <div class="grid-x grid-padding-x align-top">
+        <div class="cell small-12 video-container"></div>
+    </div>
+
+    <div class="separator-center">&nbsp;</div>
+
+    <div class="grid-x grid-padding-x align-top">
+        <div class="cell small-12 medium-4">
+            <b>WHO WE ARE</b>
+        </div>
+        <div class="cell small-12 medium-8">
+            <p>As a Design Studio we strongly believe that every concept creation is way beyond beautiful images and
+                catchy
+                phrases.</p>
+
+            <p>Design is science, where every element is carefully planned based on the ideas it has to represent and
+                then
+                implemented into a concept where every single piece is strategically placed to
+                serve a specific function in making your Brand unique.</p>
+
+            <p>We use an intellectual approach and our Creativity is based on experience and qualifications of our team.
+                Our
+                way of Concept Development is technological, scientific and almost mathematical.
+                Therefore reasoning behind every dot and pixel can be explained.</p>
+
+            </p>Nothing is left to chance and nothing is random.<br/>
+            Every single image serves a purpose.<br/>
+            Every pattern is thoroughly planned.<br/>
+            Every color has a meaning.<br/>
+            Every word is a statement.<br/>
+            </p>
+            Our Projects cover <a href="#">Web Design</a>, <a href="#">Graphic Design</a>, <a href="#">Brand
+                Development</a>,
+            <a href="#">Branding and further support</a>.
         </div>
     </div>
 
-    @push('inline-scripts')
-        <script src="{{asset('js/flickity.min.js')}}"></script>
-        <script>
-            const elem = document.querySelector('.main-carousel')
-            const flkty = new Flickity(elem, {
-                cellAlign: 'center',
-                initialIndex: 2,
-                contain: true,
-                wrapAround: true,
-                groupCells: true,
-                autoPlay: 5000,
-                pauseAutoPlayOnHover: true,
-                prevNextButtons: false,
-            })
-        </script>
-    @endpush
+    <div class="separator-center">&nbsp;</div>
 
-    @if(count($allPosts) > 0)
-        <div class="section">
-            @foreach($allPosts as $post)
-
-                @if(count($post['posts']) > 0)
-                    <div class="section-title mb-4 mt-3 mt-sm-0">
-                        <h1>{{$post['category']->title}}</h1>
-                    </div>
-                    @include ('posts/_list', ['posts' => $post['posts']])
-                @endif
-            @endforeach
+    <div class="grid-x grid-padding-x">
+        <div class="cell small-12 medium-12" style="padding: .7rem 1rem;">
+            <b>ALL PROJECTS</b>
         </div>
-    @endif
+        <div class="cell small-12 medium-12">
+            <ul class="dropdown menu gray flex-dir-row-reverse" data-dropdown-menu>
+                <li>
+                    <a href="#0">Branding</a>
+                </li>
+                <li>
+                    <a href="#0">Web Design</a>
+                </li>
+                <li>
+                    <a href="#0">Logo</a>
+                </li>
+                <li>
+                    <a href="#0">Advertising</a>
+                </li>
+                <li>
+                    <a href="#0">Packaging</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="separator-center">&nbsp;</div>
+
+    <div class="grid-x grid-padding-x">
+        <div class="cell small-6 in-view-x" data-translate="Y" data-from="600" data-to="0">
+            <a href="#" class="grayed">
+                <img src="/images/projects/SideHustle_Seltzer_Futu_Packeging.png">
+            </a>
+            <br/> <br/>
+            <b>Side Hustle Brews</b><br/>
+            <b>Brewery. Brooklyn USA</b><br/>
+            <span class="annotation">Packaging Design + Promo Materials</span> <br/>
+            <span class="annotation">2019</span>
+        </div>
+        <div class="cell small-6 in-view-x" data-from="-500" data-top="0">
+            <a href="#" class="grayed">
+                <img src="/images/projects/IG_gallery.png">
+            </a>
+            <br/> <br/> <b>IG Gallery</b><br/>
+            <b>Art Gallery. Paris FRANCE</b><br/>
+            <span class="annotation">Posters + Promo Materials</span><br/>
+            <span class="annoKation">2016</span>
+        </div>
+    </div>
+
+    <div class="separator-center">&nbsp;</div>
+
+    <div class="grid-x grid-padding-x">
+        <div class="cell small-6 in-view-x" data-from="-300" data-top="0">
+            <a href="#" class="grayed">
+                <img src="/images/projects/Odin_hospitality.png">
+            </a>
+            <br/> <br/>
+            <b>ODIN Hospitality Solutions</b><br/>
+            <b>Hospitality Solutions Company. Dubai UAE</b><br/>
+            <span class="annotation">Branding Concept</span> <br/>
+            <span class="annotation">2019</span>
+        </div>
+        <div class="cell small-6 in-view-p in-view-x" data-translate="Y" data-from="400" data-top="0">
+            <a href="#" class="grayed">
+                <img src="/images/projects/Chief_de_sucre.png">
+            </a>
+            <br/> <br/>
+            <b>Chef de Sucre</b><br/>
+            <b> Bakery. KINGDOM OF SAUDI ARABIA</b><br/>
+            <span class="annotation"> Logo + Promo Materials + Branding Concept</span><br/>
+            <span class="annotation">2020</span>
+        </div>
+    </div>
+
+    <div class="separator-center">&nbsp;</div>
+
+    <div class="grid-x grid-padding-x" style="align-items: center;  justify-content: center;">
+        <div class="cell small-6 in-view-x" data-translate="Y">
+            <a href="#" class="grayed">
+                <img src="/images/projects/Die4rocknroll.png">
+            </a>
+            <br/> <br/>
+            <b>die4Rocknroll</b><br/>
+            <b> Publisher. London UK</b><br/>
+            <span class="annotation">Book Covers + Book Layouts + Web Design</span> <br/>
+            <span class="annotation">2015</span>
+        </div>
+        <div class="cell small-6 in-view-x" data-from="-400" data-to="0" style="text-align: center;">
+            <b><a href="#" class="gray-link">More Projects</a></b>
+        </div>
+    </div>
+
+    <div class="separator-center">&nbsp;</div>
+    <div class="separator-center">&nbsp;</div>
+
+    <div class="grid-x grid-padding-x gray-background">
+        <div class="cell medium-7 large-7 hide-for-small-only hide-for-medium-only"
+             style="overflow: hidden; display: flex;">
+            <div class="white-line">&nbsp;</div>
+            <img src="/images/500.png" class="mckinley">
+        </div>
+        <div class="cell medium-12 large-5 text-right" style="padding: 25px; padding-bottom: 10px;">
+            <h1>McKINLEY LOGO EXPRESS</h1>
+            <br>
+            <h3>YOU can start by getting <br> a professional LOGO</h3>
+            <br>
+            <h3 style="font-weight: bold; font-size: 1.5rem;">1 Logo 1 Week 1 McKINLEY</h3>
+            <br>
+            <h3 class="gray">*Small Business and StartUps ONLY</h3>
+        </div>
+    </div>
+
+    <div class="separator-center">&nbsp;</div>
+    <div class="separator-center">&nbsp;</div>
+
+    <div class="grid-x grid-padding-x align-top">
+        <div class="cell small-12">
+            <b>OUR CLIENTS</b>
+        </div>
+    </div>
+
+    <div class="separator-center">&nbsp;</div>
+
+    <div class="grid-x grid-margin-x grid-padding-x align-center img-clients in-view-x" data-from="-300" data-to="0">
+        <div class="cell auto">
+            <img src="/images/clients/CheckDeSucre.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/HustleFest.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/Kant.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/Never2serious.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/Madonna.png"/>
+        </div>
+    </div>
+
+    <div class="separator-center">&nbsp;</div>
+
+    <div class="grid-x grid-margin-x grid-padding-x align-center img-clients in-view-x" data-from="500" data-to="0">
+        <div class="cell auto">
+            <img src="/images/clients/Calipso.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/UVO.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/RadicalRuhm.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/iowa.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/Pravda.png"/>
+        </div>
+    </div>
+
+    <div class="separator-center">&nbsp;</div>
+
+    <div class="grid-x grid-margin-x grid-padding-x align-center img-clients in-view-x" data-translate="Y"
+         data-from="-200"
+         data-to="0">
+        <div class="cell auto">
+            <img src="/images/clients/Aurora.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/Superjet.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/VeganCafe.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/Academy.png"/>
+        </div>
+        <div class="cell auto">
+            <img src="/images/clients/igro-market.png"/>
+        </div>
+    </div>
+
+
+    {{--    <div class="page-header mt-5">--}}
+    {{--        <div class="row">--}}
+    {{--            <div class="col-lg-9 mb-5 mb-sm-0 mb-md-0">--}}
+    {{--                <div class="section">--}}
+    {{--                    <div class="section-title">--}}
+    {{--                        <h1>@lang('posts.last_posts')</h1>--}}
+    {{--                    </div>--}}
+    {{--                    <div class="section-items main-carousel">--}}
+    {{--                        @foreach($posts as $post)--}}
+    {{--                            <div class="section-item carousel-cell">--}}
+    {{--                                @include('posts/_card')--}}
+    {{--                            </div>--}}
+    {{--                        @endforeach--}}
+    {{--                    </div>--}}
+    {{--                </div>--}}
+    {{--            </div>--}}
+    {{--            <div class="col-lg-3 mb-2">--}}
+    {{--                @include ('posts/_search_form')--}}
+
+    {{--                <span class="d-sm-none d-lg-block d-md-block d-xl-block">--}}
+    {{--                    @include('posts/_tags')--}}
+    {{--                </span>--}}
+    {{--            </div>--}}
+    {{--        </div>--}}
+    {{--    </div>--}}
+
+    {{--    @push('inline-scripts')--}}
+    {{--        <script src="{{asset('js/flickity.min.js')}}"></script>--}}
+    {{--        <script>--}}
+    {{--            const elem = document.querySelector('.main-carousel')--}}
+    {{--            const flkty = new Flickity(elem, {--}}
+    {{--                cellAlign: 'center',--}}
+    {{--                initialIndex: 2,--}}
+    {{--                contain: true,--}}
+    {{--                wrapAround: true,--}}
+    {{--                groupCells: true,--}}
+    {{--                autoPlay: 5000,--}}
+    {{--                pauseAutoPlayOnHover: true,--}}
+    {{--                prevNextButtons: false,--}}
+    {{--            })--}}
+    {{--        </script>--}}
+    {{--    @endpush--}}
+
+    {{--    @if(count($allPosts) > 0)--}}
+    {{--        <div class="section">--}}
+    {{--            @foreach($allPosts as $post)--}}
+
+    {{--                @if(count($post['posts']) > 0)--}}
+    {{--                    <div class="section-title mb-4 mt-3 mt-sm-0">--}}
+    {{--                        <h1>{{$post['category']->title}}</h1>--}}
+    {{--                    </div>--}}
+    {{--                    @include ('posts/_list', ['posts' => $post['posts']])--}}
+    {{--                @endif--}}
+    {{--            @endforeach--}}
+    {{--        </div>--}}
+    {{--    @endif--}}
+
+
 
 @endsection
---
+++ resources/views/shared/home_nav.blade.php
@@ -0,0 +1,105 @@
+<div id="top"></div>
+
+<div class="fullscreen" style="display: none;">
+    <div class="dot"></div>
+</div>
+<div style=" position: fixed; left: -20px; top: 30%; height:
+                50vh; width: 15px; background-color: #fcd436; border: 0;
+                border-top-right-radius: 20px 20px; border-bottom-right-radius: 20px 20px;
+z-index: 14; opacity: 0;" type="button"
+     data-toggle="offCanvasLeft1" class="off-c">
+    <div style="
+    color: #FFFFFF;
+    position: absolute;
+    top: 42%;
+    left: 25%;">
+        ||
+    </div>
+</div>
+
+<div class="off-canvas-wrapper">
+    <div class="off-canvas position-left" id="offCanvasLeft1" data-off-canvas>
+        <div style="margin-top: 30%;">
+            <ul class="vertical menu drilldown"
+                data-drilldown
+                data-auto-height="true"
+                data-animate-height="true"
+                data-back-button="<li class=js-drilldown-back><a tabindex=0> << </a></li>">
+                <li><a href="#">One</a></li>
+                <li>
+                    <a href="#">Two</a>
+                    <ul class="menu vertical nested">
+                        <li><a href="#">Two A</a></li>
+                        <li><a href="#">Two B</a></li>
+                        <li><a href="#">Two C</a></li>
+                        <li><a href="#">Two D</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Three</a></li>
+                <li><a href="#">Four</a></li>
+            </ul>
+        </div>
+        <div style="position: absolute; bottom: 0; width: 100%;">
+            <div class="cell auto menu-socials" style="text-align: center;">
+                <ul>
+                    <li>
+                        <a href="#"><img src="/images/menu-instagram.svg"></a>
+                    </li>
+                    <li>
+                        <a href="#"> <img src="/images/menu-facebook.svg"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="/images/menu-linkedin.svg"></a>
+                    </li>
+                    <li>
+                        <a href="#"> <img src="/images/menu-behance.svg"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="separator-center show-for-large show-for-medium show-for-landscape">&nbsp;</div>
+
+<div data-sticky-container>
+    <div class="grid-x grid-padding-x align-top top-menu hide-for-small-only" data-sticky
+         data-options="marginTop: 0;"
+         data-top-anchor="top"
+         data-stick-to="top">
+        <div class="cell small-12">
+            <div data-responsive-toggle="responsive-menu" data-hide-for="medium">
+                <button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
+                <div class="title-bar-title">Menu</div>
+            </div>
+
+            <div class="top-bar" id="responsive-menu">
+                <div class="top-bar-left">
+                    <ul class="dropdown menu" data-dropdown-menu>
+                        <li>
+                            <a href="#0">Home</a>
+                        </li>
+                        <li>
+                            <a href="#0">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#0">Express</a>
+                        </li>
+                        <li>
+                            <a href="#0">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="top-bar-right">
+                    <ul class="menu">
+                        <li>
+                            <img src="/images/logo.svg" alt="?" width="100%" height="100%">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="separator-center">&nbsp;</div>
